c6aaf9c7eab29795c1edf39bd278dee5c905dfd5
Barbacoa08 <sabe12070@gmail.com>
2022-07-16 09:01:53 -0700

    add astro blog (basic only, no real code) (#1)
    * add astro blog
    
    "just the basic" and nothing else ("no" to both questions)
    
    * get astro working properly (and automatically)
    
    * use clearer syntax
    
    * fix tsconfig warning
    
    * add build steps for netlify
    
    necessary for using PNPM w/ Netlify
    
    * adding in a recommended "prebuild" script for netlify
    
    * test 1
    
    * adjust netlify build command (again)
    
    cripes this is annoying... might need to switch over to vercel
    
    * `prebuild` script doesn't seem to be doing anything useful
    
    * :grimace: risky risky...
    
    * is the cached actually used?

---
+++ src/apps/blog/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), svelte()],
+  vite: {
+    server: {
+      open: true,
+    },
+  },
+});
