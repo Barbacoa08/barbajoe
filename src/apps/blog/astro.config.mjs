bf86a504d600413fff2b5fd44e91c3b4ca9f700b
JoeS <sabe12070@gmail.com>
2022-07-15 19:23:53 -0700

    get astro working properly (and automatically)

--- src/apps/blog/astro.config.mjs
+++ src/apps/blog/astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), svelte()],
+  vite: {
+    server: {
+      open: true,
+    },
+  },
+});
