ee585655d53b835776b11380e4d95b8d23e82cd0
JoeS <sabe12070@gmail.com>
2022-07-17 07:50:41 -0700

    add `react-lib`

---
+++ src/packages/react-lib/vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "react-lib",
+      fileName: "react-lib",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
