67413a6cc76869e06fd5d8da94065ff50a8a1347
JoeS <sabe12070@gmail.com>
2022-09-08 13:21:33 -0700

    add 404 page

--- src/apps/blog/astro.config.mjs
+++ src/apps/blog/astro.config.mjs
@@ -4,6 +4,7 @@ import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: 'https://barbajoe.tech/',
   adapter: netlify(),
   integrations: [react(), svelte()],
   vite: {
