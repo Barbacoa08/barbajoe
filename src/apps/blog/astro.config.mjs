e8d08ac41998a7924cc9456305b7eb1806de6b7f
barbajoe <sabe12070@gmail.com>
2022-09-08 15:41:17 -0500

    add 404 pages for blog+landing  (#23)
    * add 404 page
    
    * config touchups
    
    * improve readability of 404 pages

--- src/apps/blog/astro.config.mjs
+++ src/apps/blog/astro.config.mjs
@@ -4,7 +4,9 @@ import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://blog.barbajoe.tech/",
   adapter: netlify(),
+  output: "server",
   integrations: [react(), svelte()],
   vite: {
     server: {
