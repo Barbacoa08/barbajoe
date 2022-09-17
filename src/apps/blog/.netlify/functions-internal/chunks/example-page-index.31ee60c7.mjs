9504e813a59fd538d58931b17eb7eda617fd9911
JoeS <sabe12070@gmail.com>
2022-09-17 09:50:02 -0700

    confused as to why these were generated and if I need to include them

---
+++ src/apps/blog/.netlify/functions-internal/chunks/example-page-index.31ee60c7.mjs
@@ -0,0 +1,11 @@
+const Card_astro_astro_type_style_index_0_lang = '';
+
+const LinkCardGrid_astro_astro_type_style_index_0_lang = '';
+
+const styles = {
+	"fancy-btn": "_fancy-btn_c4aj6_1"
+};
+
+const Counter_svelte_svelte_type_style_lang = '';
+
+export { styles as s };
