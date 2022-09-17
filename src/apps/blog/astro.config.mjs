270dd786573a053f868e0249b2fe2bb56ae10e4d
barbajoe <sabe12070@gmail.com>
2022-09-17 12:17:48 -0500

    fix blog site (hopefully) (#26)
    * tweak `netlify.toml` for more accurate functionality
    
    * remove netlify SSR due to it not working as expected 
    
    only ever rendered a Netlify 404 page and couldn't figure out why, so not worrying about SSR for now

--- src/apps/blog/astro.config.mjs
+++ src/apps/blog/astro.config.mjs
@@ -1,12 +1,9 @@
-import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://blog.barbajoe.tech/",
-  adapter: netlify(),
-  output: "server",
   integrations: [react(), svelte()],
   vite: {
     server: {
