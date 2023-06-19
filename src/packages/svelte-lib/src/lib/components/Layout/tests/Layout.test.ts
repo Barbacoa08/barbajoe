96c7129491a4385989bde87ca2ec49fc4d7a7c06
barbajoe <sabe12070@gmail.com>
2023-06-19 16:30:34 -0500

    testing is all working!

---
+++ src/packages/svelte-lib/src/lib/components/Layout/tests/Layout.test.ts
@@ -0,0 +1,20 @@
+import { render, fireEvent, screen } from "@testing-library/svelte";
+
+import LayoutWithHeaderAndFooter from "./LayoutWithHeaderAndFooter.svelte";
+import MinimalLayout from "./MinimalLayout.svelte";
+
+describe("Layout", () => {
+  it("LayoutWithHeaderAndFooter: fully renders without exploding", () => {
+    render(LayoutWithHeaderAndFooter);
+
+    const element = screen.getByText("Layout With Header And Footer");
+    expect(element).toBeInTheDocument();
+  });
+
+  it("MinimalLayout: fully renders without exploding", () => {
+    render(MinimalLayout);
+
+    const element = screen.getByText("Minimal Layout");
+    expect(element).toBeInTheDocument();
+  });
+});
---
+++ src/packages/svelte-lib/src/lib/components/Layout/tests/LayoutWithHeaderAndFooter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Layout } from "..";
+</script>
+
+<Layout>
+  <svelte:fragment slot="header">barbajoe's svelte library</svelte:fragment>
+
+  <h1>Layout With Header And Footer</h1>
+
+  <svelte:fragment slot="footer">footer as empty fragment slot</svelte:fragment>
+</Layout>
