8b9679d11956e31d718220869c08d1e90bcab9a4
barbajoe <sabe12070@gmail.com>
2023-06-19 15:55:18 -0500

    update svelte library and integrate with staging app (#80)
    * feat: Add initial files for Svelte library
    
    This commit adds the initial files for a Svelte library, including configuration files (.eslintrc.cjs, .eslintignore, .gitignore, .npmrc, .prettierignore, and .prettierrc), package.json with devDependencies and peerDependencies, README.md file with basic instructions on how to create and develop a project using this library. It also includes new directories (src/lib/components/Layout and src/lib/components/Link) with their respective index.ts and svelte files. Finally, it adds an example page (+page.svelte) that uses the Layout component to display some text and an internal link.
    
    * update root `package.json` with more+better keywords
    
    * update eslint rules to allow svelte files
    
    * replace old svelte-lib with new one
    
    started with a fresh install of the lib based on the Svelte docs
    
    * update root staging scripts to startup all libraries
    
    * integrate svelte-lib with staging app
    
    * better test name

---
+++ src/packages/svelte-lib/src/lib/components/Layout/Layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Footer, Header } from "./";
+  import { Link } from "../Link";
+</script>
+
+<Header><slot name="header">Header</slot></Header>
+
+<main id="main-content">
+  <slot />
+</main>
+
+<Footer>
+  <slot name="footer">
+    &forall; by <Link isExternal href="https://github.com/Barbacoa08/">
+      <code>barbajoe</code>
+    </Link>
+  </slot>
+</Footer>
---
+++ src/packages/svelte-lib/src/lib/components/Layout/index.ts
@@ -0,0 +1,8 @@
+import Layout from "./Layout.svelte";
+export { Layout };
+
+import Header from "./Header.svelte";
+export { Header };
+
+import Footer from "./Footer.svelte";
+export { Footer };
