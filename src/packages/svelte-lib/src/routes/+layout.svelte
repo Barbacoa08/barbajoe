c58978b58b3a82fe0402122f226a5da369db0501
barbajoe <sabe12070@gmail.com>
2023-06-21 18:01:10 -0500

    add a few quick examples
    should probably start using storybook

---
+++ src/packages/svelte-lib/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Layout, Link } from "$lib";
+  import type { HeaderLink } from "$lib/types";
+
+  const links: HeaderLink[] = [
+    { href: "/page1", text: "Page 1" },
+    { href: "/page2", text: "Page 2" },
+    { href: "/page3", text: "Page 3" },
+  ];
+</script>
+
+<Layout {links}>
+  <svelte:fragment slot="header">
+    <Link>barbajoe's svelte library</Link>
+  </svelte:fragment>
+
+  <slot />
+
+  <svelte:fragment slot="footer">footer as empty fragment slot</svelte:fragment>
+</Layout>
