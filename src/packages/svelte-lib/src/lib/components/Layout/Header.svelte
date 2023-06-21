928d7e856b69e88d2e97edd2210b6d5a5392aeec
barbajoe <sabe12070@gmail.com>
2023-06-21 18:00:34 -0500

    improve Header component (and Layout component)

--- src/packages/svelte-lib/src/lib/components/Layout/Header.svelte
+++ src/packages/svelte-lib/src/lib/components/Layout/Header.svelte
@@ -1,5 +1,22 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import type { HeaderLink } from "$lib/types";
+
+  export let links: HeaderLink[] = [];
+</script>
+
 <header>
   <a class="skip-link" href="#main-content">Skip to main content</a>
 
   <slot />
+
+  {#if links.length > 0}
+    <ul>
+      {#each links as link}
+        <li class:active={$page.url.pathname.startsWith(link.href)}>
+          <a href={link.href}>{link.text}</a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
 </header>
--- src/packages/svelte-lib/src/lib/components/Layout/Layout.svelte
+++ src/packages/svelte-lib/src/lib/components/Layout/Layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import type { HeaderLink } from "$lib/types";
+
   import { Footer, Header } from "./";
   import { Link } from "../Link";
+
+  export let links: HeaderLink[] = [];
 </script>
 
-<Header><slot name="header">Header</slot></Header>
+<Header {links}><slot name="header">Header</slot></Header>
 
 <main id="main-content">
   <slot />
