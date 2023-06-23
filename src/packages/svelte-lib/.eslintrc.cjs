af1921baa57cb2a445f870c63df34422ac9239b6
barbajoe <sabe12070@gmail.com>
2023-06-22 20:43:29 -0500

    pnpx sb@next init

--- src/packages/svelte-lib/.eslintrc.cjs
+++ src/packages/svelte-lib/.eslintrc.cjs
@@ -1,30 +1,23 @@
 module.exports = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:svelte/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended", "prettier", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
-    extraFileExtensions: [".svelte"],
+    extraFileExtensions: [".svelte"]
   },
   env: {
     browser: true,
     es2017: true,
-    node: true,
+    node: true
   },
-  overrides: [
-    {
-      files: ["*.svelte"],
-      parser: "svelte-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-      },
-    },
-  ],
+  overrides: [{
+    files: ["*.svelte"],
+    parser: "svelte-eslint-parser",
+    parserOptions: {
+      parser: "@typescript-eslint/parser"
+    }
+  }]
 };
