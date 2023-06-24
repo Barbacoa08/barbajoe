85118c22a8e40ce45b4ca84919854a6e2670ec3d
barbajoe <sabe12070@gmail.com>
2023-06-24 18:50:57 -0500

    always use the css-lib in storybook

--- src/packages/svelte-lib/.storybook/preview.ts
+++ src/packages/svelte-lib/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/svelte";
 
+import "css-lib/dist/barbiecss.css";
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
