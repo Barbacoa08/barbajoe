e933f5f9c2dfda0396f74748136fc642c32f49dc
barbajoe <sabe12070@gmail.com>
2023-06-24 17:56:59 -0500

    organize story files, add a11y test, autodoc stories

--- src/packages/svelte-lib/src/stories/Header/Header.stories.ts
+++ src/packages/svelte-lib/src/stories/Header/Header.stories.ts
@@ -4,8 +4,6 @@ import Header from "./Header.svelte";
 const meta = {
   title: "Example/Header",
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/svelte/configure/story-layout
     layout: "fullscreen",
