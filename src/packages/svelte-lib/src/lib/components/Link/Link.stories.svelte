2e5ee7361bdf4bcd0a9e5dbefbc45c64f639f903
barbajoe <sabe12070@gmail.com>
2023-07-23 15:24:51 -0500

    add Svelte Stories! (#110)
    * basic idea
    
    * update deps
    
    * revert storybook from `7.1.0` to `7.0.0`
    
    there's a bug in Svelte CSRF that for `7.1.0`, but they are actively working on it
    
    * ignore storybook output when linting
    
    * sveltekit doesn't understand storybook stories
    
    ignore stories for the sveltekit build as we don't care about them in that context anyway
    
    * quick touch up
    
    * remove comment

---
+++ src/packages/svelte-lib/src/lib/components/Link/Link.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+
+  import Link from "./Link.svelte";
+</script>
+
+<Meta title="Components/Link" component={Link} />
+
+<Template let:args>
+  <Link {...args} />
+</Template>
+
+<Story name="Auto args" args={{ href: "https://kagi.com" }}>ping</Story>
+
+<Story name="Link">
+  <Link href="https://kagi.com">Kagi Search Engine</Link>
+</Story>
