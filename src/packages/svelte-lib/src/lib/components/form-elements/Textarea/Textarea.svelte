4b5c340817d093a48316a9b0aa2834ad3d8ee88f
barbajoe <sabe12070@gmail.com>
2024-06-15 17:50:48 -0500

    update form element styling (#224)
    * move label into div
    
    * remove dupe styling
    
    * combine to simplify
    
    * bump css and svelte libs
    
    * cleanup

--- src/packages/svelte-lib/src/lib/components/form-elements/Textarea/Textarea.svelte
+++ src/packages/svelte-lib/src/lib/components/form-elements/Textarea/Textarea.svelte
@@ -3,40 +3,25 @@ import { genId } from "$lib/utils";
 </script>
 
 <script lang="ts">
-  export let value: string = "";
-  export let placeholder: string = "";
-  export let id: string | undefined = genId();
+	export let value: string = "";
+	export let placeholder: string = "";
+	export let id: string | undefined = genId();
 </script>
 
-<label for={id}><slot /></label>
-<textarea
-  {...$$restProps}
-  {id}
-  {value}
-  {placeholder}
-  on:click
-  on:blur
-  on:focus
-  on:change
-  on:input
-  on:keydown
-  on:keyup
-  on:paste
-/>
-
-<style>
-  label {
-    font-size: larger;
-    font-weight: bold;
-  }
-
-  textarea {
-    font-size: var(--font-size-base);
-    box-sizing: border-box;
-    width: 100%;
-    margin-top: 0.1rem;
-    border: 1px solid var(--border-color-gray);
-    border-radius: 0.2em;
-    padding: 0.3em;
-  }
-</style>
+<div>
+	<label for={id}><slot /></label>
+	<textarea
+		{...$$restProps}
+		{id}
+		{value}
+		{placeholder}
+		on:click
+		on:blur
+		on:focus
+		on:change
+		on:input
+		on:keydown
+		on:keyup
+		on:paste
+	/>
+</div>
